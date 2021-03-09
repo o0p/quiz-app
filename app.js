fabc850eef5db54bb4e12c6672e9c7c1fe69633b
Russell <russelldev@protonmail.com>
2021-03-09 14:40:59 +0000

    Add quiz score counters

--- app.js
+++ app.js
@@ -41,3 +41,6 @@ const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
